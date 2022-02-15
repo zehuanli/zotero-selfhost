9109af21c1d693eb423009948dfca677cd8e5dcb
Zehuan Li <22104836+zehuanli@users.noreply.github.com>
2022-02-15 04:55:27 +0000

    Remove use of SNS

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 sudo docker-compose exec app-zotero bash -c 'cd /var/www/zotero/misc && ./init-mysql.sh'
-sudo docker-compose exec app-zotero bash -c 'aws --endpoint-url "http://localstack:4575" sns create-topic --name zotero'
